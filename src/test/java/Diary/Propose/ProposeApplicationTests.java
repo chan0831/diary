fd0f42269a9e4c76512f615f5bf000095b89420c
이찬우 <ichan-u@ichan-uui-MacBookAir.local>
2024-12-09 22:16:10 +0900

    파일옮기기 완료

---
+++ src/test/java/Diary/Propose/ProposeApplicationTests.java
@@ -0,0 +1,13 @@
+package Diary.Propose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
